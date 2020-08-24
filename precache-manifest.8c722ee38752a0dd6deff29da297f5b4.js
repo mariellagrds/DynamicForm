1f73f10676d7ef5397abfde7a24d216b7385bdd5
Mariella Gerodias <mariellagrds@gmail.com>
2020-08-24 21:30:48 +0800

    Updates

---
+++ precache-manifest.8c722ee38752a0dd6deff29da297f5b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8206e2caee14e2afe9edd1eb2f0087",
+    "url": "/DynamicForm/index.html"
+  },
+  {
+    "revision": "cf1bab526dd8bf8ece33",
+    "url": "/DynamicForm/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bd551c08c5895bc5ed0d",
+    "url": "/DynamicForm/static/css/main.7b263a55.chunk.css"
+  },
+  {
+    "revision": "cf1bab526dd8bf8ece33",
+    "url": "/DynamicForm/static/js/2.7ba0b2e9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/DynamicForm/static/js/2.7ba0b2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd551c08c5895bc5ed0d",
+    "url": "/DynamicForm/static/js/main.f47d99bf.chunk.js"
+  },
+  {
+    "revision": "4735b2002acb291de65b",
+    "url": "/DynamicForm/static/js/runtime-main.cbdbd072.js"
+  }
+]);
